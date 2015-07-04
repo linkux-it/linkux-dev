dbabf70279dac84fb21932168ab3ff28eedd82e1
Luis Carlos Cruz Carballo <lcruzc@linkux-it.com>
2015-07-04 19:13:31 -0400

    Installer for ubuntu ready

--- installers/ubuntu/basic.sh
+++ installers/ubuntu/basic.sh
@@ -29,25 +29,6 @@ done
 
 chsh -s /bin/zsh
 
-e_header "Install base16 themes"
-[ ! -d "~/.config/base16-shell" ] && git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
-
-e_header "Update .zshrc for base 16"
-if grep -Fxq "# start bas16 setup" ~/.zshrc
-then
-  sed -i '/# start bas16 setup/,/# end python setup/d' ~/.zshrc
-fi
-
-cat >> ~/.zshrc <<- EOM
-# start bas16 setup
-
-# Base16 Shell
-BASE16_SHELL="$HOME/.config/base16-shell/base16-default.dark.sh"
-[[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
-
-# end bas16 setup
-EOM
-
 # Setting up neobundle
 if [ ! -d ~/.vim/bundle ]; then
   e_header "Creating bundle directory for NeoBundle"
--- installers/ubuntu/settings.sh
+++ installers/ubuntu/settings.sh
@@ -2,6 +2,8 @@
 source ./installers/common.sh
 echo
 
+e_header "Linking configurations"
+
 FILES="${$HOME}/.linkux-dev/conf/ubuntu/vimrc
 ${$HOME}/.linkux-dev/conf/ubuntu/tmux.conf
 ${$HOME}/.linkux-dev/conf/ubuntu/zpreztorc
@@ -11,4 +13,23 @@ ${$HOME}/.linkux-dev/conf/ubuntu/ackrc"
 for rcfile in $FILES; do
   ln -s "$rcfile" "${rcfile:t}"
 done
+
+e_header "Install base16 themes"
+[ ! -d "~/.config/base16-shell" ] && git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+
+e_header "Update .zshrc for base 16"
+if grep -Fxq "# start bas16 setup" ~/.zshrc
+then
+  sed -i '/# start bas16 setup/,/# end python setup/d' ~/.zshrc
+fi
+
+cat >> ~/.zshrc <<- EOM
+# start bas16 setup
+
+# Base16 Shell
+BASE16_SHELL="$HOME/.config/base16-shell/base16-default.dark.sh"
+[[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
+
+# end bas16 setup
+EOM
 
