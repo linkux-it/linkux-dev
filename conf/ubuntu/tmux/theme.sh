8b96d2f163547eafaee5b6a9e56640b2ef1e6264
Luis Carlos Cruz Carballo <lcruzc@linkux-it.com>
2015-07-05 16:09:50 -0400

    Enable battery on tmux

--- conf/ubuntu/tmux/theme.sh
+++ conf/ubuntu/tmux/theme.sh
@@ -57,5 +57,5 @@ tm_date="#[fg=$tm_color_inactive] %d %b %Y |%l:%M %p"
 tm_host="#[fg=$tm_color_feature,bold]#h@#[fg=brightblue]#(curl icanhazip.com)#[fg=yellow] #(ifconfig en0 | grep 'inet ' | awk '{print \"en0 \" $2}') #(ifconfig en1 | grep 'inet ' | awk '{print \"en1 \" $2}') #[fg=red]#(ifconfig tun0 | grep 'inet ' | awk '{print \"vpn \" $2}') "
 tm_session_name="#[fg=$tm_color_feature,bold]$tm_icon #S"
 
-set -g status-left $tm_session_name' '$tm_battery
-set -g status-right $tm_itunes' '$tm_rdio' '$tm_spotify' '$tm_date' '$tm_battery' '$tm_host
+set -g status-left $tm_battery' '$tm_session_name' '$tm_battery
+set -g status-right $tm_itunes' '$tm_rdio' '$tm_spotify' '$tm_date' '$tm_host
