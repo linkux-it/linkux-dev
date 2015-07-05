99a6373b85696ed4ec0843d2d153e9b88c935b72
Luis Carlos Cruz Carballo <lcruzc@linkux-it.com>
2015-07-05 19:19:24 -0400

    Fix hearts

--- conf/osx/sh/battery_indicator.sh
+++ conf/osx/sh/battery_indicator.sh
@@ -7,7 +7,7 @@ battery_info=`ioreg -rc AppleSmartBattery`
 current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
 total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
 
-charged_slots=$(echo "(($current_charge/$total_charge)*2)+1" | bc -l | cut -d '.' -f 1)
+charged_slots=$(echo "(($current_charge/$total_charge)*5)+1" | bc -l | cut -d '.' -f 1)
 if [[ $charged_slots -gt 5 ]]; then
   charged_slots=5
 fi
--- conf/ubuntu/sh/battery_indicator.sh
+++ conf/ubuntu/sh/battery_indicator.sh
@@ -6,15 +6,15 @@ HEART='♥ '
 current_charge=$(cat /proc/acpi/battery/BAT1/state | grep 'remaining capacity' | awk '{print $3}')
 total_charge=$(cat /proc/acpi/battery/BAT1/info | grep 'last full capacity' | awk '{print $4}')
 
-charged_slots=$(echo "((($current_charge/$total_charge)*10)/3)+1" | bc -l | cut -d '.' -f 1)
-if [[ $charged_slots -gt 3 ]]; then
-  charged_slots=3
+charged_slots=$(echo "(($current_charge/$total_charge)*5)+1" | bc -l | cut -d '.' -f 1)
+if [[ $charged_slots -gt 5 ]]; then
+  charged_slots=5
 fi
 
 echo -n '#[fg=colour196]'
 for i in `seq 1 $charged_slots`; do echo -n "$HEART"; done
 
-if [[ $charged_slots -lt 3 ]]; then
+if [[ $charged_slots -lt 5 ]]; then
   echo -n '#[fg=colour254]'
-  for i in `seq 1 $(echo "3-$charged_slots" | bc)`; do echo -n "$HEART"; done
+  for i in `seq 1 $(echo "5-$charged_slots" | bc)`; do echo -n "$HEART"; done
 fi
