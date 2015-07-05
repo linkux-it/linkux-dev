22296d1e188c4ff3cd9ff4aa98430d1d7d24f8db
Luis Carlos Cruz Carballo <lcruzc@linkux-it.com>
2015-07-05 18:14:24 -0400

    Batery saver

--- conf/osx/sh/battery_indicator.sh
+++ conf/osx/sh/battery_indicator.sh
@@ -7,15 +7,15 @@ battery_info=`ioreg -rc AppleSmartBattery`
 current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
 total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
 
-charged_slots=$(echo "((($current_charge/$total_charge)*10)/3)+1" | bc -l | cut -d '.' -f 1)
-if [[ $charged_slots -gt 3 ]]; then
-  charged_slots=3
+charged_slots=$(echo "(($current_charge/$total_charge)*10)+1" | bc -l | cut -d '.' -f 1)
+if [[ $charged_slots -gt 10 ]]; then
+  charged_slots=10
 fi
 
-echo -n '#[fg=colour196]'
+echo -n '#[fg=color196]'
 for i in `seq 1 $charged_slots`; do echo -n "$HEART"; done
 
-if [[ $charged_slots -lt 3 ]]; then
-  echo -n '#[fg=colour254]'
-  for i in `seq 1 $(echo "3-$charged_slots" | bc)`; do echo -n "$HEART"; done
+if [[ $charged_slots -lt 10 ]]; then
+  echo -n '#[fg=color254]'
+  for i in `seq 1 $(echo "10-$charged_slots" | bc)`; do echo -n "$HEART"; done
 fi
