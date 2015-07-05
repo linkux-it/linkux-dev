7f666f9102ae771f408d796531ed7545b9400ab5
Luis Carlos Cruz Carballo <lcruzc@linkux-it.com>
2015-07-04 20:15:25 -0400

    Installer for ubuntu ready

--- installers/ubuntu/basic.sh
+++ installers/ubuntu/basic.sh
@@ -37,9 +37,8 @@ ${ZDOTDIR:-$HOME}/.zprezto/runcoms/zprofile
 ${ZDOTDIR:-$HOME}/.zprezto/runcoms/zshenv
 ${ZDOTDIR:-$HOME}/.zprezto/runcoms/zshrc"
 for rcfile in $FILES; do
-	echo "${rcfile:t}" "${rcfile}"
-
-  ln -s "${rcfile:t}" "${rcfile}"
+  ln -s "${rcfile}" "$(~/.$(basename $rcfile))"
+  echo "${rcfile}" "$(~/.$(basename $rcfile))"
 done
 
 sudo chsh -s $(which zsh)
