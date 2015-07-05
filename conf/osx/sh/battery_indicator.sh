82b0bc3721abc099eb3a0fe2d130ce875bea0394
Luis Carlos Cruz Carballo <lcruzc@linkux-it.com>
2015-07-05 18:19:02 -0400

    Fix hearts

--- conf/osx/sh/battery_indicator.sh
+++ conf/osx/sh/battery_indicator.sh
@@ -7,15 +7,15 @@ battery_info=`ioreg -rc AppleSmartBattery`
 current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
 total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
 
-charged_slots=$(echo "(($current_charge/$total_charge)*5)+1" | bc -l | cut -d '.' -f 1)
+charged_slots=$(echo "(($current_charge/$total_charge)*2)+1" | bc -l | cut -d '.' -f 1)
 if [[ $charged_slots -gt 5 ]]; then
   charged_slots=5
 fi
 
-echo -n '#[fg=color196]'
+echo -n '#[fg=colour196]'
 for i in `seq 1 $charged_slots`; do echo -n "$HEART"; done
 
 if [[ $charged_slots -lt 5 ]]; then
-  echo -n '#[fg=color254]'
+  echo -n '#[fg=colour254]'
   for i in `seq 1 $(echo "5-$charged_slots" | bc)`; do echo -n "$HEART"; done
 fi
