a0a2fc3a5c73836a42c6bfaa016c004b2eca4357
Luis Carlos Cruz Carballo <lcruzc@linkux-it.com>
2015-07-07 16:32:20 -0400

    Ubuntu back old installed zsh scripts

--- installers/ubuntu/python.sh
+++ installers/ubuntu/python.sh
@@ -16,7 +16,7 @@ cat >> ~/.zshrc <<- EOM
 
 export WORKON_HOME=\$HOME/.virtualenvs
 export PROJECT_HOME=\$HOME/Projects
-export VIRTUALENVWRAPPER_SCRIPT=/usr/local/bin/virtualenvwrapper.sh
+export VIRTUALENVWRAPPER_SCRIPT=/usr/share/virtualenvwrapper/virtualenvwrapper.sh
 source /usr/local/bin/virtualenvwrapper_lazy.sh
 
 # end python setup
