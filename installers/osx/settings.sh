f46ebac4cf1da56676fc2438b66b93c1074027e6
Luis Carlos Cruz Carballo <lcruzc@linkux-it.com>
2015-12-14 22:28:06 -0400

    New theme to use in terminal

--- installers/osx/settings.sh
+++ installers/osx/settings.sh
@@ -26,7 +26,7 @@ cat >> ~/.zshrc <<- EOM
 # start global setup
 
 # Base16 Shell
-BASE16_SHELL="\$HOME/.config/base16-shell/base16-default.dark.sh"
+BASE16_SHELL="\$HOME/.config/base16-shell/base16-ashes.dark.sh"
 [[ -s \$BASE16_SHELL ]] && source \$BASE16_SHELL
 
 # tmux aliases
