202e91404b7adbdd3fd7d9aeeae3d048eaab5e78
Luis Carlos Cruz Carballo <lcruzc@linkux-it.com>
2016-10-10 14:30:46 -0400

    Move from zprezto to zim

--- installers/ubuntu/basic.sh
+++ installers/ubuntu/basic.sh
@@ -45,7 +45,8 @@ setopt EXTENDED_GLOB
 for template_file in ${ZDOTDIR:-${HOME}}/.zim/templates/* 
 do
   user_file = ${ZDOTDIR:-${HOME}}/.${template_file:t}
-  touch ${user_file}
+  echo $user_file
+  touch $user_file
   ( print -rn "$(<${template_file})$(<${user_file})" >! ${user_file} ) 2>/dev/null
 done
 
