c9e3dcb6b326a847e308383cd6192db39a7edf4e
Luis Carlos Cruz Carballo <lcruzc@linkux-it.com>
2016-10-10 14:26:12 -0400

    Move from zprezto to zim

--- installers/ubuntu/basic.sh
+++ installers/ubuntu/basic.sh
@@ -42,8 +42,9 @@ e_header "Install Zim"
 git clone --recursive https://github.com/Eriner/zim.git ${ZDOTDIR:-${HOME}}/.zim
 
 setopt EXTENDED_GLOB
-for template_file ( ${ZDOTDIR:-${HOME}}/.zim/templates/* ); do
-  user_file="${ZDOTDIR:-${HOME}}/.${template_file:t}"
+for template_file in ${ZDOTDIR:-${HOME}}/.zim/templates/* 
+do
+  user_file = ${ZDOTDIR:-${HOME}}/.${template_file:t}
   touch ${user_file}
   ( print -rn "$(<${template_file})$(<${user_file})" >! ${user_file} ) 2>/dev/null
 done
